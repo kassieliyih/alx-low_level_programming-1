9cf0481e15e1b0c67880ba8c40e8982eaeaacdec
bete7512 <betekbebe@gmail.com>
2022-03-12 08:33:02 -0800

    i am tring to fix the problem

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,22 @@
 #include <stdlib.h>
 
 /**
- *  * main - Entry point
- *   * Return: Always 1 (Success)
- *    */
+ * main - Entry point
+ * Return: Always 1 (Success)
+ */
 int main(void)
 {
-		write(STDOUT_FILENO, "and that piece of art is useful
-							\" - Dora Korpar, 2015-10-19\n", 59);
-			return (1);
+	char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	long l = 59;
+	long fd = 1;
+	long syscall = 1;
+	long ret = 0;
+
+	__asm__ ("syscall"
+			: "=a" (ret)
+			: "a" (syscall),
+			"D" (fd),
+			"S" (s),
+			"d" (l));
+	return (1);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,22 +1,16 @@
-#include <stdio.h>
 
-/*
- * main -Entry point
+#include <stdio.h>
+/**
+ * main - Prints sized of characters
  *
- * Return: Always 0 (Success)
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-	int a;
-	long int b;
-	long long int c;
-	char d;
-	float f;
-
-	printf("size of a char: %lu bytes(s)\n", (unsigned long)sizeof(d));
-	printf("size of an int: %lu bytes(s)\n", (unsigned long)sizeof(a));
-	printf("size of a long int: %lu bytes(s)\n", (unsigned long)sizeof(b));
-	printf("size of a long long int: %lu bytes(s)\n", (unsigned long)sizeof(c));
-	printf("size of a float: %lu bytes(s)\n", (unsigned long)sizeof(f));
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
 	return (0);
 }
