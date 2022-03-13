d2e7cb06576d20d48c5bef66545df46a2fd399d4
bete7512 <betekbebe@gmail.com>
2022-03-13 05:43:45 -0700

    special one

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main 
+ *
+ * Return:
+ */
+int main(void)
+{
+	char v;
+	char m;
+
+	for (v = '0'; v <= '8';v++)
+	{
+		for(m = '0'; m <= 9
+	}
+}
---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry poinoidfvhscxzjknzmjhgsdxnzbxsnt
+ *dssssssssssssssssxczzzzz
+ *Return: 0 (Success)cxzzzzzzzzzzzzzzzzzzzzzzzz
+ */
+int main(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+		if (i != '9')
+		{
+		putchar(',');
+		putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
