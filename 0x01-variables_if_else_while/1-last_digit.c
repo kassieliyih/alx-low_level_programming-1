babffecfd00608b1c5052c7cb8667789c0cec9a4
bete7512 <betekbebe@gmail.com>
2022-03-13 03:33:38 -0700

    vitea

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,7 +5,7 @@
 /**
  *main -entry point
  *
- *Return Always 0 (success)
+ *Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -18,7 +18,7 @@ int main(void)
 		printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
 	else if (n % 10 < 6)
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
-	else if (n % 10 == 0)
+	else
 		printf("Last digit of %d is %d and is 0\n", n, n % 10);
 
 	return (0);
