70d2adf71f5e42dd107938bc4eb3b7f7ca36b002
bete7512 <betekbebe@gmail.com>
2022-03-13 08:59:07 -0700

    vitea is my name

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,11 +7,21 @@
  */
 int main(void)
 {
-	char v;
-	char m;
+	int n,m;
 
-	for (v = '0'; v <= '8';v++)
+	for( n = 0; n <= 8; n++)
 	{
-		for(m = '0'; m <= 9
+		for (m = 0; m <= 9; m++)
+		{
+			putchar((n % 10) + '0');
+			putchar((m % 10) + '0');
+			if (n == 8 && m == 9)
+				continue;
+			putchar(',');
+			putchar(' ');
+
+		}
 	}
+	putchar('\n');
+	return (0);
 }
