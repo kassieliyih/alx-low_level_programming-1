35c89650792bdb874848915f3fba3132d6c0feb0
bete7512 <betekbebe@gmail.com>
2022-03-13 04:18:33 -0700

    am here for him

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char b;
+
+	for (b = 'a'; b <= 'z'; b++)
+	{
+		if (b == q || b == e)
+			continue;
+		putchar(b);
+		putchar('\n');
+	}
+	return (0);
+
+}
