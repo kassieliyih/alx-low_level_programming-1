bef0f16f181d0aed71748f828d5ea82692b13307
bete7512 <betekbebe@gmail.com>
2022-03-14 11:48:51 -0700

    vitea

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,9 +13,9 @@ int main(void)
 
 	for (i = 0; i <= 7; i++)
 	{
-		for (j = 1; j <= 8; j++)
+		for (j = i+ 1; j <= 8; j++)
 		{
-			for (k = 2; k <= 9; k++)
+			for (k = j + 1; k <= 9; k++)
 			{
 				putchar((i % 10) + '0');
 				putchar((j % 10) + '0');
