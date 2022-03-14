3adc7be81a25ad2dfd0cf0168fcecc50bca518e6
bete7512 <betekbebe@gmail.com>
2022-03-14 11:52:44 -0700

    new one

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,14 +13,14 @@ int main(void)
 
 	for (i = 0; i <= 7; i++)
 	{
-		for (j = i+ 1; j <= 8; j++)
+		for (j = i + 1; j <= 8; j++)
 		{
 			for (k = j + 1; k <= 9; k++)
 			{
 				putchar((i % 10) + '0');
 				putchar((j % 10) + '0');
 				putchar((k % 10) + '0');
-				if (i == 7 && j == 8 && k == 90)
+				if (i == 7 && j == 8 && k == 9)
 					continue;
 				putchar(',');
 				putchar(' ');
