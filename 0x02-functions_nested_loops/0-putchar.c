88c6effdec4a7accc4fdbe500faa1e3717fda4cc
bete7512 <betekebe@gmail.com>
2022-03-16 12:19:17 +0300

    am here for them

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char m[] ="_putchar";
+	int i=0;
+
+	for (i = 0; i <= 7; i++)
+	{
+		_putchar(m[i]);
+	}
+	_putchar('\n');
+	return 0;
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+STill am here
