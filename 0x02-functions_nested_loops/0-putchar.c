ca14592cb14d8070885728d35ef4678a7903733c
bete7512 <betekebe@gmail.com>
2022-03-16 12:22:25 +0300

    am here

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,13 +6,13 @@
  */
 int main(void)
 {
-	char m[] ="_putchar";
-	int i=0;
+	char m[] = "_putchar";
+	int i = 0;
 
 	for (i = 0; i <= 7; i++)
 	{
 		_putchar(m[i]);
 	}
 	_putchar('\n');
-	return 0;
+	return (0);
 }
