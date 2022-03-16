e4317452f3c3fc174b1e5993217dfe1a506a9820
bete7512 <betekebe@gmail.com>
2022-03-16 20:03:29 +0300

    99 error makes you 100% man

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,7 @@
+#include "main.h"
+
+
+void print_to_98(int n)
+{
+
+}
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,23 +12,21 @@ void times_table(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j <= 9; j++)
+		_putchar('0');
+		for (j = 1; j <= 9; j++)
 		{
-		if ((i * j) > 9)
-		{
-			_putchar(((i * j) / 10) + '0');
-			 _putchar(((i * j) % 10) + '0');
-		}
-		else
-		{
+			_putchar(',');
 			_putchar(' ');
-			_putchar((i * j) + '0');
-
-		}
-		if (j == 9)
-			continue;
-		 _putchar(',');
-		  _putchar(' ');
+			if ((i * j) > 9)
+			{
+				_putchar(((i * j) / 10) + '0');
+				_putchar(((i * j) % 10) + '0');
+			}
+			else
+			{
+				 _putchar(' ');
+				 _putchar((i * j) + '0');
+			}
 		}
 		_putchar('\n');
 
