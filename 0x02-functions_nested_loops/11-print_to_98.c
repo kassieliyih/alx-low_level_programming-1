b1f8534a396949ce86a0fc30442ee3a1ee122ae8
bete7512 <betekebe@gmail.com>
2022-03-16 20:53:57 +0300

    just error 100 times in a day

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,17 +13,17 @@ void print_to_98(int n)
 
 	if (n > 98)
 	{
-		for (i = n; i >= 98; i--)
+		for (i = n; i > 98; i--)
 			printf("%d, ", i);
-		printf("\n");
+		printf("%d \n", 98);
 
 	}
 	else
 	{
-		for (i = n; i <= 98; i++)
+		for (i = n; i < 98; i++)
 		{
 			printf("%d, ", i);
 		}
-		printf("\n");
+		printf("%d \n", 98);
 	}
 }
