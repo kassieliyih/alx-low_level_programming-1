91f87e70e09ae192998a86762502c7d834695244
bete7512 <betekebe@gmail.com>
2022-03-17 09:28:34 +0300

    sometimes try in blind

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - causes an infinite  loop
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("Infiniteloop incoming :(\n)";/* the errorr is here*/
+
+	i = 0;
+
+	while (i < 10)
+	{
+	       putchar(i);
+	}
+
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}
