c10d9f7f39290aec7dde7c6543210c738d923ff2
bete7512 <betekebe@gmail.com>
2022-03-17 09:15:34 +0300

    at the middle of error there exists inspiration and success

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-void positive_or_negative(int i);
+#include <stdio.h>
+void positive_or_negative(int);
 
 #endif /* MAIN_H */
---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success);
+ */
+void positive_or_negative(int n)
+{
+
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	else
+		printf("%d is negative\n", n);
+
+}
