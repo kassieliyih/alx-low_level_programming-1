d384bc848e56c47f1ce75ed21cf709f1d00638e1
bete7512 <betekebe@gmail.com>
2022-03-17 15:03:35 +0300

    every commit haves a value for your error

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *mul - this function is made for returnnig integer
+ *@a: this is paramter for function mul to do its role
+ *@b: this is
+ *Return: integre
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
