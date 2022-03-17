b3929931fab0156fdc4cffb42b5bc255e19ff50f
bete7512 <betekebe@gmail.com>
2022-03-17 14:40:36 +0300

    just fun for fun

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,19 +1,15 @@
 #include "main.h"
 /**
- *
- *
- *
+ *_isupper - this functio
+ *@c: this is parameter fot _upper function
+ *Return: integer vale
  *
  */
 
 int _isupper(int c)
 {
-	if ( c >= 65 && c <= 90)
+	if (c >= 65 && c <= 90)
 	{
-		/*(putchar(c);
-		*putchar(':');
-		*putchar(' ');
-		*putchar('1');*/
 		return (1);
 	}
 	else
