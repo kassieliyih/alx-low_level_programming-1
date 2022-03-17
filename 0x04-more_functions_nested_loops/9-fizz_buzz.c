4d2b99f9b731d1a36bf8dcc1d966d71687998aed
bete7512 <betekebe@gmail.com>
2022-03-17 21:30:06 +0300

    just mistake

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,13 +11,16 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0)
-			printf("Fizz ");
+			printf("Fizz");
 		else if (i % 5 == 0)
-			printf("buzz ");
+			printf("Buzz");
 		else if ((i % 3 == 0) && (i % 5 == 0))
-			printf("fizzbuzz ");
+			printf("FizzBuzz");
 		else
-			printf("%d ", i);
+			printf("%d", i);
+		if (i == 100)
+			continue;
+		printf(" ");
 	}
 	printf("\n");
 	return (0);
