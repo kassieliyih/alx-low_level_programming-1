44901ececf22f2a3226e642be44e4be0180aac97
bete7512 <betekebe@gmail.com>
2022-03-18 21:48:38 +0300

    an other errror

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -7,7 +7,7 @@
  *@month: month in number format
  *@day: day of month
  *@year: year
-Return: void
+ *Return: void
  *
  */
 void print_remaining_days(int month, int day, int year)
@@ -17,7 +17,7 @@ void print_remaining_days(int month, int day, int year)
 		if (month >= 3 && day >= 60)
 		{
 			day++;
-	
+
 		}
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 366 - day);
