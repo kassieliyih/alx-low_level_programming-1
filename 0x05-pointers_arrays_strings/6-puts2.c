8dc2de99bf3abd0c0e718ba73bc7501126940085
bete7512 <betekbebe@gmail.com>
2022-03-21 16:57:01 -0400

    am tring to do my tasks with myself

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *puts2 - callee
+ *@str:parameter
+ *
+ */
+
+void puts2(char *str)
+{
+	int i;
+	int j = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+		continue;
+	i = i - 1;
+	for (j = 0; j <= i; j = j + 2)
+	{
+		_putchar(str[j]);
+	}
+}
