b86f8f205647c673bd2c891eeaa762a50aa95e1f
bete7512 <betekbebe@gmail.com>
2022-03-21 17:22:52 -0400

    am tring to do my tasks with myself

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -18,6 +18,8 @@ void puts_half(char *str)
 	length_of_the_string = n;
 	if (length_of_the_string % 2 != 0)
 		n = (length_of_the_string - 1) / 2;
+	else
+		n = length_of_the_string / 2;
 	for (i = n + 1; i <= length_of_the_string; i++)
 	{
 		_putchar(str[i]);
