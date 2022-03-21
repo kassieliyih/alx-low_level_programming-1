f743dcf2236e530a349b5b5d2c0d392f91ecf928
bete7512 <betekbebe@gmail.com>
2022-03-21 03:58:57 -0400

    today is new day

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *swap_int - this is function to swap th
+ *@a:parameter 1
+ *@b:parameter2
+ */
+
+void swap_int(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,2 +1,3 @@
 void reset_to_98(int *n);
 char _putchar(char c);
+void swap_int(int *a, int *b);
