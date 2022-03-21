3c56b1be25815d48e7eb52297968ec0ad76604ba
bete7512 <betekbebe@gmail.com>
2022-03-21 17:19:17 -0400

    am tring to do my tasks with myself

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *puts_half - just the way
+ *@str:parameter
+ *
+ *
+ */
+void puts_half(char *str)
+{
+	int n;
+	int i;
+	int length_of_the_string;
+
+	for (n = 0; str[n] != '\0'; n++)
+		continue;
+	n = n - 1;
+	length_of_the_string = n;
+	if (length_of_the_string % 2 != 0)
+		n = (length_of_the_string - 1) / 2;
+	for (i = n + 1; i <= length_of_the_string; i++)
+	{
+		_putchar(str[i]);
+	}
+}
+
