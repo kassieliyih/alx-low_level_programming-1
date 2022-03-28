eb18c59074a7296ce7b8442638fc595afebac71c
bete7512 <betekbebe@gmail.com>
2022-03-28 12:35:18 -0400

    pointer array string

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *_memset - function for memory setting
+ *@s:char
+ *@b:char
+ *@n:int
+ *Return:char
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned char *p = s;
+
+	for (; n > 0; n--)
+	{
+		*p++ = (unsigned char) b;
+	}
+	return (s);
+}
