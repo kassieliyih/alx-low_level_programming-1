da177dad867d852b177752614dc9346a6954d04a
bete7512 <betekbebe@gmail.com>
2022-03-29 00:41:31 -0400

    pointer array string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *_strchar - function for strring
+ *@s:char
+ *@b:char
+ *Return:ff
+ */
+
+
+char *_strchar(char *s, char b)
+{
+	for (; *s != '\0'; ++s)
+		if (*s == b)
+		{
+			return (s);
+		}
+	return (NULL);
+}
