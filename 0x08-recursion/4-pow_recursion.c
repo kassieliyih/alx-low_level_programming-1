e06c4bfb694f4a22cf60fa1c593a1ada957914ff
bete7512 <betekbebe@gmail.com>
2022-03-30 16:11:19 -0400

    recursion task for alx softaware engineering

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,14 +1,25 @@
 #include "main.h"
 
 /**
- *
- *
- *
- *
+ *_pow_recursion - entry
+ *@x:info
+ *@y:int
+ *Return:mm
  *
  */
 
 int _pow_recursion(int x, int y)
 {
+	int k;
+
+	if (y < 0)
+		return (-1);
+	else if (y != 0)
+	{
+		k = x * _pow_recursion(x, y - 1)
+	}
+	else
+		return (1);
+	return (k);
 
 }
