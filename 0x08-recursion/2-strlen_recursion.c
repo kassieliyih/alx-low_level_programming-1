0dd0f13350323d8e443448be09dce004b73820c9
bete7512 <betekbebe@gmail.com>
2022-03-30 15:39:52 -0400

    recursion task for alx softaware engineering

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+
+/**
+ *_strlen_recursion - entry
+ *@s:
+ *Return:int
+ *
+ *
+ *
+ */
+
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+	
+	if (*s)
+	{
+		i++;
+		i += _strlen_recursion(s + 1);
+	}
+	return (i);
+}
