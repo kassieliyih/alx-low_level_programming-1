a3fea34380be9ece2adcee78fd66195fc838e5b1
bete7512 <betekbebe@gmail.com>
2022-03-30 14:53:14 -0400

    pointer array string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *_puts_recursion - is the function
+ *@s:char
+ *
+ *
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\0');
+
+}
