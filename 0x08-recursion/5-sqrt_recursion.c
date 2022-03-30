fc919f3c77e72a35a83da89941f4ae60cc9ca9ca
bete7512 <betekbebe@gmail.com>
2022-03-30 16:28:51 -0400

    recursion task for alx softaware engineering

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,21 +1,17 @@
 #include "main.h"
 
 /**
- *
- *
- *
- *
- *
- */
-
-int _sqrt_recursion(int n)
-{
-/**
  * check - checks for the square root
  * @a:int
  * @b:int
  *
  * Return: int
+ */
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: integer to find sqrt of
+ * Return: natural square root or -1
  */
 int check(int a, int b)
 {
@@ -26,9 +22,10 @@ int check(int a, int b)
         return (check(a + 1, b));
 }
 
-/**
- * _sqrt_recursion - returns the natural square root of a number
- * @n: integer to find sqrt of
- * Return: natural square root or -1
- */
+int _sqrt_recursion(int n)
+{
+        if (n == 0)
+                return (0);
+        return (check(1, n));
+}
 
