6d100dd7f7c599f376136d7c11ec9266eeca9d2e
bete7512 <betekbebe@gmail.com>
2022-04-01 18:36:21 -0400

    arguments to main functions

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 
 /**
- *
- *
- *
+ *main - entry
+ *@argc:int
+ *@argv:char
+ *Return:int
  */
 
 int main(int argc, char *argv[])
 {
 	int i;
 
-	for (i = 0;i < argc; i++)
+	for (i = 0; i < argc; i++)
 	{
-		printf("%s\n",argv[i]);
+		printf("%s\n", argv[i]);
 	}
 	return (0);
 }
