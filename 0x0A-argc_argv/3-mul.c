c5ee0d13d987597704ad49c6cf310b22203bfca3
bete7512 <betekbebe@gmail.com>
2022-04-01 18:57:55 -0400

    arguments to main functions

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "stdlib.h"
+
+/**
+ * main - entry point
+ * @argc:point
+ * @argv:char
+ * Return:int
+ */
+
+int main(int argc, char* argv[])
+
+{
+	int mul;
+	int x, z;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else{
+		x = atoi(argv[1]);
+		z = atoi(argv[2]);
+		mul = x * z;
+		printf("%d\n", mul);
+	}
+	return (0);
+}
