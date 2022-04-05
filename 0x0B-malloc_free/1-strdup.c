8d979050d15f57e152ff380b3559af6d6454789f
bete7512 <betekbebe@gmail.com>
2022-04-05 08:14:42 -0400

    this is my first static library

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ *_strdup - function
+ *@str:int
+ *Return:char
+ *
+ *
+ *
+ */
+
+char *_strdup(char *str)
+{
+	char *new;
+	int i;
+	int holder;
+
+	if (str == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		for (i = 0; str[i] != '\0'; i++)
+			continue;
+		holder = i - 1;
+		new = malloc(sizeof(char) * (holder));
+		if (new == NULL)
+		{
+			return (0);
+		}
+		else
+		{
+			for (i = 0; i <= holder; i++)
+			{
+				new[i] = str[i];
+			}
+		}
+	}
+	new[holder + 1] = '\0';
+	return (new);
+}
