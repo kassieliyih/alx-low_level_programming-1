4144593194fbabbd7653da959e96da027c9d2497
bete7512 <betekbebe@gmail.com>
2022-04-05 16:49:36 -0400

    this is my first static library

--- 0x09-static_libraries/lib/2-strchr.c
+++ 0x09-static_libraries/lib/2-strchr.c
@@ -1,18 +1,30 @@
 #include "main.h"
+
 /**
- *_strchr - function for strring
- *@s:char
- *@b:char
- *Return:ff
+ * _strchr - locates a character in a string
+ * @s: string
+ * @c: character
+ *
+ * Return: pointer to the first occurence of the character
+ * or NULL if character is not found
  */
 
-
-char *_strchr(char *s, char b)
+char *_strchr(char *s, char c)
 {
-	for (; *s; ++s)
-		if (*s == b)
+	unsigned int index, length;
+
+	length = 0;
+
+	while (s[length] != '\0')
+		length++;
+
+	for (index = 0; index <= length; index++)
+		if (s[index] == c)
 		{
-			break;
+			s = &s[index];
+			return (s);
 		}
-	return (s);
+
+	return ('\0');
 }
+
