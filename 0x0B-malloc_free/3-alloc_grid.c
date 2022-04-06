4d0da9ece099986c6b32485dd28d75a3c5cb0d17
bete7512 <betekbebe@gmail.com>
2022-04-06 13:06:15 -0400

    this is my first static library

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -23,7 +23,13 @@ int **alloc_grid(int width, int height)
 	for (i = 0; i < height; i++)
 	{
 		array[i] = (int *)malloc(width * sizeof(int));
-
+		if (array[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+				free(array[j]);
+			free(array);
+			return (NULL);
+		}
 		for (j = 0; j <= width; j++)
 		{
 			array[i][j] = 0;
