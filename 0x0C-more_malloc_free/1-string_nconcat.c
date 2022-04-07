9837ca2e81d599cad6c7182471ac533109cbb943
bete7512 <betekbebe@gmail.com>
2022-04-07 12:16:07 -0400

    this is my first static library

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -30,7 +30,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		return (NULL);
 	for (i = 0; s1[i] != '\0'; i++)
 		m[i] = s1[i];
-	for (j = 0; j < n && holder1; j++)
+	for (j = 0; j <= n && holder1; j++)
 		*(m + holder1++) = s2[j];
 	m[holder1] = '\0';
 	return (m);
